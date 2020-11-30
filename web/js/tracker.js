52834069c6cad77ffde973c9e75f1a72943dce12
ChaudharyShok <chaudharyShok.com>
2020-11-30 13:54:59 +0530

    adding final commit

--- web/js/tracker.js
+++ web/js/tracker.js
@@ -1,3 +1,16 @@
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+webcam.start()
+  .then(result =>{
+    console.log("webcam started");
+  })
+  .catch(err => {
+    console.log(err);
+});
+
+
 var money =document.getElementById("cash");
 var fail = false;
 var walletHistory = [];
