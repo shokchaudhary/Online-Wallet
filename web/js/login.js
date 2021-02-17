5438516f74cc86cee340c5d689512d1a523dbe9e
ChaudharyShok <chaudharyShok.com>
2021-02-17 12:30:07 +0530

    changing urls

--- web/js/login.js
+++ web/js/login.js
@@ -2,13 +2,16 @@ function validateForm(username , password)
 {
   if(username.value ==  "rahihacker@hack.in" && password.value == "divya@2020")
   {
+    // var p = document.getElementById("warning");
+    // p.style.color = 'lightgreen';
+    // p.innerHTML = "<a href='./option2.html'>Visit Destination </a>";
+    // p.setAttribute("id" , "nextpage");
+    // var a = document.querySelectorAll("#nextpage a")[0];
+    // a.style.color = "lightgreen";
+    // a.style.textDecoration = "none";
     var p = document.getElementById("warning");
-    p.style.color = 'lightgreen';
-    p.innerHTML = "<a href='./option2.html'>Visit Destination </a>";
-    p.setAttribute("id" , "nextpage");
-    var a = document.querySelectorAll("#nextpage a")[0];
-    a.style.color = "lightgreen";
-    a.style.textDecoration = "none";
+    p.style.color = 'red';
+    p.innerHTML = "Invalid Username OR Password";
   }
   else
   {
